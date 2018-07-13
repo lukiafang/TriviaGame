ce20394edb9d92b6d0864910d69442c4c5cc2cc6
lukia <447227675@qq.com>
2018-07-13 02:15:10 -0400

    7/13

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+
+//set timer countdown& timeout session 
+var count =60,
+timer = setInterval(function()
+   {
+       $("#counter").html(count--);
+       console.log(counter)
+       if(count == 0 ) clearInterval(timer);
+
+   }, 1000 
+);
+// Set the date we're counting down to
+
+
+//once timeup display result 
+
+
+//
+function submitAnswer() {
+    var radios = document.getElementsByName("choice");
+    var i = 0, len = radios.length;
+    var checked = false;
+    var userAnswer;
+    
+    for( ; i < len; i++ ) {
+       if(radios[i].checked) {
+         checked = true;
+         userAnswer = radios[i].value;
+       }
+    } 
+    // if user click submit button without selecting any option, alert box should be say "please select choice answer".
+    if(!checked) {
+      alert("please select choice answer");
+      return;
+    }
+    // Correct answer
+    if(userAnswer === "Scripting") {
+       alert("Answer is correct!");
+    }
+    // incorrect answer
+    else {
+       alert("Answer is wrong!");
+    }
+    
+  }
